bdd006d1e915d3fee1705303835b5df64d490f58
Leo-Emmanuel <leoemmanuel2004@gmail.com>
2025-07-15 23:58:47 +0530

    Added Login component

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+const [input,changeInput]=useState(
+    {username:"",password:""}
+)
+const inputHandler=(event)=>{
+    changeInput({...input,[event.target.name]:event.target.value})
+}
+const readvalues=()=>{
+    console.log(input)
+}
   return (
     <div>
         <div className="container">
@@ -10,14 +19,14 @@ const Login = () => {
                         
                             <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                                 <label htmlFor="" className="form-label">Username</label>
-                                <input type="text" className="form-control" />
+                                <input type="text" className="form-control" name='username' value={input.username} onChange={inputHandler}/>
                             </div>
                             <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                                 <label htmlFor="" className="form-label">Password</label>
-                                <input type="text" className="form-control" />
+                                <input type="text" className="form-control" name='password' value={input.password}onChange={inputHandler} />
                             </div>
                             <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                                <button className="btn btn-success">Button</button>
+                                <button className="btn btn-success" onClick={readvalues}>Button</button>
                             </div>
                         
                     </div>
